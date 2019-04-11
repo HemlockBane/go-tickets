b9b86322db3bcbace0d92623520c037c347b0fb0
Obinna Igwe <igwenus619@gmail.com>
2019-04-11 15:59:31 +0100

    Remove print statements

--- lib/src/widgets/time_tool.dart
+++ lib/src/widgets/time_tool.dart
@@ -3,38 +3,43 @@ import 'package:intl/intl.dart';
 String formatTime(String timeInMillisecs) {
   String formattedTime = '';
   DateTime dateTime =
-      DateTime.fromMillisecondsSinceEpoch(int.parse(timeInMillisecs));
+  DateTime.fromMillisecondsSinceEpoch(int.parse(timeInMillisecs));
   print('time_tool.dart - fed in ${formatDate(dateTime: dateTime)}');
 
   Duration timeDifference = DateTime.now().difference(dateTime);
 
-  if (timeDifference.inHours >= 24 && timeDifference.inHours < 47) {
-    print(
-        'time_tool.dart - time difference in hours: ${timeDifference.inHours}');
-    formattedTime = 'Yesterday';
-  } else if (timeDifference.inHours > 47) {
-    print(
-        'time_tool.dart - time difference in hours: ${timeDifference.inHours}');
-    formattedTime = formatDate(dateTime: dateTime);
-    //formattedTime = 'Before yesterday';
-  } else if (timeDifference.inHours <= 23) {
-    print(
-        'time_tool.dart - time difference in min: ${timeDifference.inMinutes}');
+  if (timeDifference.inHours <= 23) {
     if (timeDifference.inHours < 1) {
       if (timeDifference.inMinutes < 1) {
         formattedTime = 'Just now';
-      } else if (timeDifference.inMinutes >= 1) {
+      }
+      else
+      if (timeDifference.inMinutes >= 1) {
         formattedTime = '${timeDifference.inMinutes} min';
       }
-    } else if (timeDifference.inHours >= 1 && timeDifference.inHours <= 5) {
+    }
+    else
+    if (timeDifference.inHours >= 1 && timeDifference.inHours <= 5) {
       formattedTime = '${timeDifference.inHours} h';
-    } else if (timeDifference.inHours > 5) {
+    }
+    else
+    if (timeDifference.inHours > 5) {
       if (dateTime.hour >= 0 && dateTime.hour <= 11) {
         formattedTime = 'Today ${dateTime.hour} a.m.';
-      } else if (dateTime.hour > 11 && dateTime.hour <= 23) {
+      }
+      else
+      if (dateTime.hour > 11 && dateTime.hour <= 23) {
         formattedTime = 'Today ${dateTime.hour} p.m.';
       }
     }
+  }
+  else
+  if (timeDifference.inHours >= 24 && timeDifference.inHours <= 47) {
+    formattedTime = 'Yesterday';
+  }
+  else
+  if (timeDifference.inHours >= 48) {
+    formattedTime = formatDate(dateTime: dateTime);
   }
   return formattedTime;
 }
@@ -44,12 +49,17 @@ String formatDate(
   String formattedTime = "";
   String formatPattern = 'dd/MM/yyyy';
   if (isDateTime) {
-    if (dateTime == null) return "";
+    if (dateTime == null)
+      return "";
+
     formattedTime = DateFormat(formatPattern).format(dateTime);
-  } else {
-    if (messageDateString == null) return "";
+  }
+  else {
+    if (messageDateString == null)
+      return "";
+
     DateTime dateTime =
-        DateTime.fromMillisecondsSinceEpoch(int.parse(messageDateString));
+    DateTime.fromMillisecondsSinceEpoch(int.parse(messageDateString));
     formattedTime = DateFormat(formatPattern).format(dateTime);
   }
 
@@ -59,10 +69,10 @@ String formatDate(
 bool isTimeDifferenceAboveEightMinutes(
     {String previousTime, String currentTime}) {
   DateTime previousDateTime =
-      DateTime.fromMillisecondsSinceEpoch(int.parse(previousTime));
+  DateTime.fromMillisecondsSinceEpoch(int.parse(previousTime));
 
   DateTime currentDateTime =
-      DateTime.fromMillisecondsSinceEpoch(int.parse(currentTime));
+  DateTime.fromMillisecondsSinceEpoch(int.parse(currentTime));
 
   Duration timeDifference = currentDateTime.difference(previousDateTime);
 
